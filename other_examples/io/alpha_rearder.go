3a67e149a49db4a6be83424c7b159415582890fb
Vladimir Vivien <vladimir.vivien@gmail.com>
2017-09-12 19:25:05 -0400

    Added an updated, better  alphaReader

---
+++ other_examples/io/alpha_rearder.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"io"
+)
+
+// alphaReader is a simple implementation of an io.Reader
+// that streams only alpha chars from its string source.
+type alphaReader struct {
+	src string
+	cur int
+}
+
+func newAlphaReader(src string) *alphaReader {
+	return &alphaReader{src: src}
+}
+
+func alpha(r byte) byte {
+	if (r >= 'A' && r <= 'Z') || (r >= 'a' && r <= 'z') {
+		return r
+	}
+	return 0
+}
+
+func (a *alphaReader) Read(p []byte) (int, error) {
+	if a.cur >= len(a.src) {
+		return 0, io.EOF
+	}
+
+	x := len(a.src) - a.cur
+	n, bound := 0, 0
+	if x >= len(p) {
+		bound = len(p)
+	} else if x <= len(p) {
+		bound = x
+	}
+
+	buf := make([]byte, bound)
+	for n < bound {
+		if char := alpha(a.src[a.cur]); char != 0 {
+			buf[n] = char
+		}
+		n++
+		a.cur++
+	}
+	copy(p, buf)
+	return n, nil
+}
+
+func main() {
+	reader := newAlphaReader("Hello! It's 9am, where is the sun?")
+	p := make([]byte, 4)
+	for {
+		n, err := reader.Read(p)
+		if err == io.EOF {
+			break
+		}
+		fmt.Print(string(p[:n]))
+	}
+	// or use io.Copy
+	// io.Copy(os.Stdout, reader)
+	fmt.Println()
+}
