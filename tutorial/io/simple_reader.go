6026ec9dd43f733fbd921b0e38f1acce184be85f
Vladimir Vivien <vladimirvivien@users.noreply.github.com>
2017-09-20 08:37:00 -0400

    fix err handling.

--- tutorial/io/simple_reader.go
+++ tutorial/io/simple_reader.go
@@ -4,15 +4,22 @@ import (
 	"fmt"
 	"io"
 	"strings"
+	"os"
 )
 
 func main() {
 	reader := strings.NewReader("Clear is better than clever")
 	p := make([]byte, 4)
+	
 	for {
 		n, err := reader.Read(p)
-		if err == io.EOF {
+		if err != nil{
+		    if err == io.EOF {
+			fmt.Println(string(p[:n]))
 			break
+		    }
+		    fmt.Pritnln(err)
+		    os.Exit(1)
 		}
 		fmt.Println(string(p[:n]))
 	}
