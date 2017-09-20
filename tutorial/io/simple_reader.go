b8bd1f07f0e03da64b6ce4f33c8a92eae8d889dd
Vladimir Vivien <vladimirvivien@users.noreply.github.com>
2017-09-20 08:39:31 -0400

    Minor fix.

--- tutorial/io/simple_reader.go
+++ tutorial/io/simple_reader.go
@@ -15,10 +15,10 @@ func main() {
 		n, err := reader.Read(p)
 		if err != nil{
 		    if err == io.EOF {
-			fmt.Println(string(p[:n]))
+			fmt.Println(string(p[:n])) //should handle any remainding bytes.
 			break
 		    }
-		    fmt.Pritnln(err)
+		    fmt.Println(err)
 		    os.Exit(1)
 		}
 		fmt.Println(string(p[:n]))
